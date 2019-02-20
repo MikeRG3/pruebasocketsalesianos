763492602db0eb1ee2e6b110b5e6377ce13d67a3
mikerg3 <mrodriguezg@salesianospizarrales.com>
2019-02-20 17:15:23 +0100

    s

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+process.env.PORT = process.env.PORT || 3000;
+
+
 
 app.get('/', function(req, res) {
     res.sendFile(__dirname + '/index.html');
 });
+
+app.set('port', process.env.PORT);
 
 io.on('connection', function(socket) {
     socket.on('chat message', function(msg) {
